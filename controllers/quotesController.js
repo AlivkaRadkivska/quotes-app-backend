3e339c1d547f0dcc12f028045e74df3cf3407d5a
AlivkaRadkivska <134500026+AlivkaRadkivska@users.noreply.github.com>
2024-08-12 13:07:39 +0300

    refactor: change response types

--- controllers/quotesController.js
+++ controllers/quotesController.js
@@ -2,13 +2,11 @@ const Quote = require('./../models/quoteModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getQuotes = catchAsync(async (req, res, next) => {
-  const items = await Quote.find({});
+  const data = await Quote.find({});
 
   res.status(201).json({
-    status: 'success',
-    data: {
-      items,
-    },
+    success: true,
+    data,
   });
 });
 
@@ -16,25 +14,27 @@ exports.addQuote = catchAsync(async (req, res, next) => {
   const { text, author } = req.body;
   if (!text | !author)
     res.status(301).json({
-      status: 'failure',
+      success: false,
       message: 'Text or author not found',
     });
 
-  const item = await Quote.create({ text, author });
+  const data = await Quote.create({ text, author });
   res.status(201).json({
-    status: 'success',
-    data: {
-      item,
-    },
+    success: true,
+    data,
   });
 });
 
 exports.deleteQuote = catchAsync(async (req, res, next) => {
-  const item = await Quote.findByIdAndDelete(req.params.id);
+  const data = await Quote.findByIdAndDelete(req.params.id);
 
-  if (!item) res.status(404).json({ status: 'Not found' });
+  if (!data)
+    res.status(404).json({
+      success: false,
+      message: 'Not found',
+    });
 
   res.status(201).json({
-    status: 'success',
+    success: true,
   });
 });
